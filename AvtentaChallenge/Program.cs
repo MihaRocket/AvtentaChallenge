ee4f3c380a4c1f7ba22ed2b3a7549663dd416eb8
Miha Nabergoj <nabergoj.miha@gmail.com>
2021-09-21 19:43:47 +0200

    Initial check-in of module AvtentaChallenge

---
+++ AvtentaChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvtentaChallenge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
