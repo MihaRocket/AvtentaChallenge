86ac9143ae7ebcd337ed994cefdaa4b762796852
mihasersenia <miha@sersenia.com>
2021-09-23 13:42:33 +0200

    Added web request and deserilization of results

--- AvtentaChallenge/Program.cs
+++ AvtentaChallenge/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using DemoWS.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace AvtentaChallenge
 {
     class MainClass
     {
+        public static string SourceURL = "http://195.88.82.191/DemoWS/Services/SourceWs.svc";
+        public static string TargetURL = "http://195.88.82.191/DemoWS/Services/DestinationWs.svc";
         public static void Main(string[] args)
         {
-            var a = "Test";
+            Akcija responseList = new Akcija();
+            SourceWsClient client = new SourceWsClient("BasicHttpBinding_ISourceWs",SourceURL);
+            client.Open();
+            var response = client.GetActions();
+            client.Close();
+            var serializer = new XmlSerializer(typeof(List<Akcija>)); 
+            using (TextReader reader = new StringReader(response))
+            {
+                List<Akcija> result = (List<Akcija>)serializer.Deserialize(reader);
+            }
         }
     }
 }
