a171d18bacd99dd8056d362d426fa194fa97f927
Miha Nabergoj <nabergoj.miha@gmail.com>
2021-09-26 09:43:48 +0200

    Added more comments

--- AvtentaChallenge/Program.cs
+++ AvtentaChallenge/Program.cs
@@ -19,15 +19,15 @@ namespace AvtentaChallenge
         public static string DestinationURL = "http://195.88.82.191/DemoWS/Services/DestinationWs.svc";
         public static void Main(string[] args)
         {
-            // Initialize xml serializer variable with object type Akcija
+            // Initializes xml serializer variable with object type Akcija
             var serializerXML = new XmlSerializer(typeof(List<Akcija>));
 
             Console.WriteLine($"Input source URL (default URL: {SourceURL}), leave blank if you want to use default URL:");
             var clintSourceURL = Console.ReadLine();
 
             /*
-             Initialize TextReader variable with data from source.
-             Initialize List with object Akcija and populates data from deserializing TextReader 
+             Initializes TextReader variable with data from source.
+             Initializes List with object Akcija and populates data from deserializing TextReader 
             */
             Console.Clear();
             Console.WriteLine("Getting data from source....");
@@ -40,10 +40,10 @@ namespace AvtentaChallenge
             var clintDestinationURL = Console.ReadLine();
 
 
-            //Submit List of data to destination  
+            //Submits List of data to destination  
             Console.Clear();
             Console.WriteLine("Submitting data to destination...");
-            SumbitDataToDestination(clintDestinationURL, resultList);
+            SumbitDataToDestination(clintDestinationURL, "nabergoj.miha@gmail.com", resultList);
             Console.WriteLine("Data successfully submitted");
 
             Console.ReadLine();
@@ -52,6 +52,12 @@ namespace AvtentaChallenge
         {
             try
             {
+                /*
+                 Initializes source client with provided URL
+                 Opens client and calls SOAP web service to get data
+                 Closes clent and returns a response string
+                */
+
                 SourceWsClient client = new SourceWsClient("BasicHttpBinding_ISourceWs", string.IsNullOrEmpty(clintSourceURL) ? SourceURL : clintSourceURL);
                 client.Open();
                 var response = client.GetActions();
@@ -64,13 +70,18 @@ namespace AvtentaChallenge
                 return null;
             }
         }
-        private static void SumbitDataToDestination(string clintDestinationURL, List<Akcija> result)
+        private static void SumbitDataToDestination(string clintDestinationURL, string Email, List<Akcija> data)
         {
             try
             {
+                /*
+                 Initializes destination client with provided URL
+                 Opens destination and submits transformed List to Array of data to destination
+                */
+
                 DestinationWsClient destination = new DestinationWsClient("BasicHttpBinding_IDestinationWs", string.IsNullOrEmpty(clintDestinationURL) ? DestinationURL : clintDestinationURL);
                 destination.Open();
-                var response = destination.SubmitActions(result.ToArray(), "nabergoj.miha@gmail.com");
+                var response = destination.SubmitActions(data.ToArray(), Email);
                 if (response != "ok")
                     throw new Exception($"Response from submit: {response}");
                 destination.Close();
@@ -82,6 +93,12 @@ namespace AvtentaChallenge
         }
         private static void ExcetionHandling(string ExcetionMessage, string MethodName)
         {
+            /*
+             When exception occures:
+                Excetion message is displayed
+                Program closes
+            */
+            
             Console.Clear();
             Console.WriteLine($"Error in method GetDataFromSource \n Message: {ExcetionMessage}");
             Console.WriteLine("Press any key to continue....");
